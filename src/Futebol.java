754b27abd4b19b42af528c303a7b6cdc5059383f
eutiagosantos <128329482+eutiagosantos@users.noreply.github.com>
2025-04-24 15:45:24 +0000

    feat: implementacao da funcao maiorTimePossivel no arquivo `Futebol.java`

--- src/Futebol.java
+++ src/Futebol.java
@@ -1,7 +1,7 @@
 package src;
 
 import java.util.Scanner;
-
+import java.util.Arrays;
 /*
  * Problema:
  * 
@@ -37,9 +37,68 @@ public class Futebol {
 
         System.out.println(maxTeamSize);
     }
+  
+    /*
+                                            EXPLICAÇÃO COMPLEXIDADE
 
+      Essa proposta de solução tem uma complexidade de tempo O(n log n), o mergesort foi o algoritimo implementado
+      dos que eu conheço é o unico que no pior caso a complexidade de tempo ia ser menor que n^2
+
+      Ordenei o array de skills e usei uma tecnica para definir subarrays com restrição (janela deslizante)
+      Foi um dos unicos que eu achei pesquisando que faria sentido nessa pratica
+    */
+    
+    private static void sort(int[] arr){
+      if(arr == null || arr.length < 2) return;
+      
+      int mid = arr.length / 2;
+      int left[] = new int[mid];
+      int right[] = new int[arr.length - mid];
+
+      sort(left);
+      sort(right);
+      
+      merge(arr, left, right);
+    }
+    
+    private static void merge(int[] arr,int[] left,int[] right){
+        int k=0, j = 0, i = 0;
+
+        while (i < left.length && j < right.length) {
+          if (left[i] <= right[j]) {
+            arr[k] = left[i];
+            i++;
+          } else {
+            arr[k] = right[j];
+            j++;
+          }
+          k++;
+        }
+        while (i < left.length) {
+          arr[k] = left[i];
+          i++;
+          k++;
+        }
+        while (j < right.length) {
+          arr[k] = right[j];
+          j++;
+          k++;
+        }
+	}
+  
     private static int maiorTimePossivel(int[] skills) {
-        // Implementar a lógica para calcular o maior time possível
-        throw new UnsupportedOperationException("Não implementado");
+        
+        sort(skills);
+
+        int amountOfTeams = 0;
+        int j = 0;
+        
+        for(int i=0;i<skills.length;i++){
+          while(Math.abs(skills[j] - skills[i]) > 5){
+            j++;
+          }
+          amountOfTeams = Math.max(amountOfTeams, i - j + 1);
+        }
+        return amountOfTeams;
     }
 }
